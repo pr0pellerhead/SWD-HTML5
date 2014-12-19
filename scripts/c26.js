62132eeabf7b09a2611da8cc0cfc5ccb9d42801f
pr0pellerhead <bojan.gavrovski@gmail.com>
2014-12-19 12:58:13 +0100

    Responsive websites (media queries), Javascript basics

---
+++ scripts/c26.js
@@ -0,0 +1,24 @@
+// ime = "Pero";
+// console.log(ime);
+// a = 4;
+// b = 5;
+// c = a + b;
+// console.log(c);
+
+grad1 = 'Bitola';
+grad2 = 'Skopje';
+
+if(grad1.length > grad2.length){
+
+	console.log('grad1 ima povekje bukvi');
+
+}else if(grad1.length == grad2.length){
+
+	console.log('grad 1 i grad 2 imaat ist broj na bukvi');
+
+}else{
+
+	console.log('grad2 ima povekje bukvi');
+	
+}
+	
